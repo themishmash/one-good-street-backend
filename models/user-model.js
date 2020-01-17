01feddc025ffb061165919cea4b9144621217842
Michelle <wong.mich.c@gmail.com>
2020-01-17 15:42:46 +1100

    search by category done

--- models/user-model.js
+++ models/user-model.js
@@ -10,7 +10,7 @@ const userSchema = new Schema({
   username: 
   {
     type: String,
-    unique: true
+   
   },
   password:
   {
--- routes/search-routes.js
+++ routes/search-routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-//const { searchByCategory, searchByLocation } = require("../controllers/item-controller");
+const { searchByCategory } = require("../controllers/item-controller");
 
 //here we are going to create routes
-// router.get("/item/:category", searchByCategory);
+router.get("/items/:category", searchByCategory);
 // router.get("/item/:location", searchByLocation);
 
 module.exports = router;
