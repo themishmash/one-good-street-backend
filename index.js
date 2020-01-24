166a0f3b870589a27f4136cc54e4503b143a3f7d
Molly Madden <molly.annette.madden@gmail.com>
2020-01-24 13:28:11 +1100

    added code for nodemailer

--- index.js
+++ index.js
@@ -7,6 +7,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+// const creds = require('dotenv').config();
+// const nodemailer = require('nodemailer');
+
 
 require('dotenv').config();
 
@@ -41,3 +44,54 @@ app.listen(PORT,
 );
 
 ///why do i have to have mongodb in .env for it to work????
+
+//START CODE FOR EMAIL
+
+
+
+
+
+// const transport = {
+//     host: 'smtp.example.com', // Don’t forget to replace with the SMTP host of your provider
+//     port: 587,
+//     auth: {
+//         user: creds.USER,
+//         pass: creds.PASS
+//     }
+// }
+
+// const transporter = nodemailer.createTransport(transport)
+
+// transporter.verify((error, success) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Server is ready to take messages');
+//     }
+// });
+
+// router.post('/send', (req, res, next) => {
+//     const name = req.body.name
+//     const email = req.body.email
+//     const message = req.body.message
+//     const content = `name: ${name} \n email: ${email} \n message: ${message} `
+
+//     const mail = {
+//         from: name,
+//         to: 'RECEIVING_EMAIL_ADDRESS_GOES_HERE',  // Change to email address that you want to receive messages on
+//         subject: 'New Message from Contact Form',
+//         text: content
+//     }
+
+//     transporter.sendMail(mail, (err, data) => {
+//         if (err) {
+//             res.json({
+//                 status: 'fail'
+//             })
+//         } else {
+//             res.json({
+//                 status: 'success'
+//             })
+//         }
+//     })
+// })
