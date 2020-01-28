8595ff7584582e879e67e8ea6b2025f4164971aa
Michelle <wong.mich.c@gmail.com>
2020-01-28 12:31:13 +1100

    nodemailer working

--- routes/send-routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { send } = require("../controllers/send-controller");
-
-router.post("/send", send);
-
-module.exports = router;
