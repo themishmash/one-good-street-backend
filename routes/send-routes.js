9e339cc985431cb6cb43450b4be389f0f855b0fb
Michelle <wong.mich.c@gmail.com>
2020-01-28 19:36:01 +1100

    contact us routes made

--- routes/send-routes.js
+++ routes/send-routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {involved} = require('../controllers/send-controller');
+const { involved, contact } = require('../controllers/send-controller');
 
 router.post('/involved', involved)
+router.post('/contact', contact)
 
 module.exports = router;
