60003e58ce1133711b496f533bc3071bdeb130fc
Michelle <wong.mich.c@gmail.com>
2020-01-28 16:57:08 +1100

    route for sending of get involved done

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.use('/items', require('./item-routes'));
 router.use('/users', require('./user-routes'));
 router.use('/search', require("./search-routes"));
 
+router.use('/send', require('./send-routes'));
 
 
 // router.get("/", require("../controllers/index"));
